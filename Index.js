9e89092934a280d7bf3bbd4345483e0a42e52475
domsjourn564-ui <domsjourn564@gmail.com>
2026-02-22 14:36:26 +0800

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,11 @@
+// File: index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import LoveGroundsReadingApp from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LoveGroundsReadingApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
